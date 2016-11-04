df1c49a93a599cd57cb2e9b694415ca4a85073a4
dtracy198 <dtracy198@gmail.com>
2016-11-04 17:16:33 -0400

    Add test that deal with clustered data.
    Coxph Model and log-rank with cluster

--- Inesfly_Paint_Bed_Bug_Trial/InesflyPaintCode.R
+++ Inesfly_Paint_Bed_Bug_Trial/InesflyPaintCode.R
@@ -224,7 +224,176 @@ cdf$"4 weeks"[missing] <- "No Data"
 #stargazer(cdf, summary = FALSE, type = "html", 
 #          out = "TABLES_GRAPHS/Bioassay_Table.htm
 write.csv(cdf,"cdf.csv")
+
 ###############################################################################
+###We have to look at quadrants
+#We have a column for quadrant(1-4), but we need to create unique columns
+#for each dish
+DataMelt$TreatQuad <- paste(DataMelt$quad, DataMelt$treatment, sep = "-")
+
+#Lets summarize the data by treatquad
+quadsum<-summaryBy(alive+dead+knockdown+unviable+living ~ TreatQuad + treatment + day 
+                        + paint + days.after.paint + exp.time,
+                        data = DataMelt, FUN=sum, na.rm=TRUE,
+                        keep.names=TRUE)
+
+quadsum$total <- (quadsum$alive + quadsum$dead 
+                       + quadsum$knockdown)
+
+##Remove rows with 0 in total (some moved pre-maturely to jars)
+no.obs <- which(quadsum$total == 0)
+quadsum <- quadsum[-no.obs,]
+
+###All bugs were alive when exposed to pesticide.  
+#remove then replace day 0 obs to show this. 
+#(Current 0 obs were made the day of treatment, after treatment)
+quads <- unique(quadsum$TreatQuad)
+zeros <- which(quadsum$day == 0)
+quadsum<- quadsum[-zeros,]
+
+#create empty data frame for 0's 
+quad.frame <- quadsum[1:length(quads),]
+#fill in numeric data and quads
+quad.frame$TreatQuad <- quads
+quad.frame$day <- quad.frame$day * 0
+quad.frame$days.after.paint <- quad.frame$day * 0
+quad.frame$alive <- quad.frame$day * 0
+quad.frame$dead <- quad.frame$day * 0
+quad.frame$knockdown <- quad.frame$day * 0
+quad.frame$unviable <- quad.frame$day * 0
+quad.frame$living <- quad.frame$day * 0
+quad.frame$total <- quad.frame$day * 0
+
+#fill in categorical data and calc total
+for(i in 1 : length(quads)) {
+  qt <- which(quadsum$TreatQuad == quads[i])
+  quad.frame$total[i]  <- quadsum$total[min(qt)]                          
+  quad.frame$paint[i]  <- quadsum$paint[min(qt)]  
+  quad.frame$treatment[i] <-quad.frame$treatment[min(qt)]
+  quad.frame$days.after.paint[i] <- quadsum$days.after.paint[min(qt)]  
+  quad.frame$exp.time[i] <- quadsum$exp.time[min(qt)]   
+}
+
+#everything is alive on day 0 so set equal to total
+quad.frame$alive  <- quad.frame$total                          
+quad.frame$living  <- quad.frame$total                          
+
+#now bind the 0 data to the treatmentsum file 
+quadsum <- rbind(quad.frame, quadsum)
+
+###Creat Proportions of each of the status colums
+quadsum$prop.alive <- quadsum$alive / quadsum$total
+quadsum$prop.dead <- quadsum$dead / quadsum$total
+quadsum$prop.kd <- quadsum$knockdown / quadsum$total
+quadsum$prop.uv <- quadsum$unviable / quadsum$total
+quadsum$prop.liv <- quadsum$living / quadsum$total
+
+#identify paints
+fap <- which(quadsum$paint == "5A")
+cop <- which(quadsum$paint == "CO")
+cfp <- which(quadsum$paint == "CF")
+
+#set pch based on paint for graph below
+quadsum$pch <- quadsum$prop.alive * 0
+quadsum$pch[fap] <- 15
+quadsum$pch[cop] <- 20 
+quadsum$pch[cfp] <- 17
+
+#identify length of exposure
+hora <- which(quadsum$exp.time == "01H")
+treshora <- which(quadsum$exp.time == "03H")
+seishora <- which(quadsum$exp.time == "06H")
+dias <- which(quadsum$exp.time == "24H")
+
+#set lty for graph below
+quadsum$lty <- quadsum$prop.alive * 0
+quadsum$lty[hora] <- 1
+quadsum$lty[treshora] <- 2 
+quadsum$lty[seishora] <- 3
+quadsum$lty[dias] <- 4
+
+
+#################################All Plot##################################
+quadsum$paint <- factor(quadsum$paint, levels = c("CO","5A","CF"))
+
+#Plot proportion alive
+par(mfrow = c(1,1)) #4 across 3 
+plot(y = quadsum$prop.alive, x = quadsum$day, 
+     pch = quadsum$pch, col = quadsum$paint, type = "n",
+     xaxt = 'n') 
+udays <- c( 0, 1, 2, 7, 14, 21, 28)
+axis(2, at =((1:9) / 10), labels = as.character(1:9 / 10))
+axis(1, at = udays, labels = udays)
+for(i in 1:length(quads)){
+  tr <- which(quadsum$TreatQuad == quads[i])
+  temp <- quadsum[tr,]
+  points(y = temp$prop.liv, x = temp$day, 
+         pch = temp$pch[1], col = temp$paint[1]   
+  )
+  lines(y = temp$prop.liv, x = temp$day, 
+        col = temp$paint[1], lty = temp$lty[1]    
+  )
+}
+
+###################################An Array####################################
+#Plot Prop Proportion living (alive+knockdown)
+quadsum$exp.time <- revalue(quadsum$exp.time, c("01H" = "1", 
+                                                     "03H" = "3", 
+                                                     "06H" = "6",
+                                                     "24H" = "24"))
+#pdf("TABLES_GRAPHS/Bioassay_Array/Bioassay_Graphs_Array_Quads.pdf", width = 6, 
+    height = 9)
+#jpeg("TABLES_GRAPHS/Bioassay_Array/Bioassay_Graphs_Array_Quads.jpeg", width = 6, 
+#     height = 9, units = "in", res = 300)
+dap <- unique(quadsum$days.after.paint)
+dap <- dap[order(dap)]
+ext <- unique(quadsum$exp.time)
+par(mfrow = c(4,3), oma = c(1,1,2,1)) #4 across 3 
+for(k in 1:length(ext)){
+  tsdap <- which(quadsum$exp.time == ext[k])
+  for(j in 1:length(dap)){
+    tsext <- which(quadsum$days.after.paint == dap[j])
+    tsde <- intersect(tsdap, tsext)  
+    #d <- "days"
+    #if(dap[j] == 1){d <- "day"}
+    plot(y = quadsum$prop.alive, x = quadsum$day, 
+         pch = quadsum$pch, col = quadsum$paint,
+         type = "n", #main = as.character(paste("J =", ext[k], "hrs:", "K =", dap[j], 
+         #            d, sep = " ")), 
+         ylab = "Proportion Alive", xlab = "Days Since Exposure", 
+         xaxt = 'n', yaxt = 'n')
+    udays <- c( 0, 7, 14, 21, 28)
+    axis( 2, at = c(0:5 / 5), las = 2,
+          labels = as.character(c(0:5 / 5)))
+    axis( 1, at = udays, labels = udays)
+    quads.tmp <- unique(quadsum$TreatQuad[tsde])
+    for(i in 1:length(quads.tmp)){
+      tr <- which(quadsum$TreatQuad == quads.tmp[i])
+      temp <- quadsum[tr,]
+      points(y = temp$prop.liv, x = temp$day, pch = temp$pch[1],
+             col = temp$paint[1])
+      lines(y = temp$prop.liv, x = temp$day, pch = temp$pch[1], 
+            col = temp$paint[1])      
+    }
+  }
+}
+
+#title
+mtext("Proportion of Alive Bugs", side = 3, line = 0, outer = TRUE, cex = 1.2)
+#Top Headings
+mtext("1 Day After Painting", side = 3, line =-2 , outer = TRUE, cex=0.8, at = 0.18) 
+mtext("90 days after painting", side = 3, line =-2 , outer = TRUE, cex=0.8, at = 0.50) 
+mtext("180 days after painting", side = 3, line =-2 , outer = TRUE, cex=0.8, at = 0.84) 
+#side headings
+mtext("Exposed 1 Hour", side = 2, line = -0.2, outer = T, at = 0.938, adj = 1, cex = 0.8) 
+mtext("Exposed 3 Hours", side = 2, line = -0.2, outer =T, at = 0.69, adj = 1, cex = 0.8) 
+mtext("Exposed 6 Hours", side = 2, line = -0.2, outer = T, at = 0.4392, adj = 1, cex = 0.8) 
+mtext("Exposed 24 Hours", side = 2, line = -0.2, outer = T, at = 0.198, adj = 1, cex = 0.8) 
+#turn off pdf or jpeg
+#dev.off() 
+
+###############################################################################
+
 ### To compare curves we need to do a log-rank test, which in R requiress us to
 ##transform data into a Survival Object
 ##This requires us to have a time (start of interval), time2(end of interval),
@@ -376,7 +545,6 @@ don <- which(IndTab$days.after.paint == 1)
 dni <- which(IndTab$days.after.paint == 90)
 doe <- which(IndTab$days.after.paint == 180)
 
-###Our study isn't appropriately powered for the above analysis.
 ##It should be powered to compare the curves between
 #5A and Control at each time point.
 #so create paint indicators
@@ -437,7 +605,7 @@ coxph(IndTab$Surv2[onfcon] ~ IndTab$paint[onfcon])
 ictest(IndTab$Surv2[onfcon] ~ IndTab$paint[onfcon], rho = 0)
 
 #Conventional Log-rank
-survdiff(IndTab$Surv[onfcon] ~ IndTab$paint[onfcon], rho = 0)
+survdiff(IndTab$Surv[onfcon] ~ IndTab$paint[onfcon])
 survdiff(IndTab$Surv[onfcth] ~ IndTab$paint[onfcth], rho = 0)
 survdiff(IndTab$Surv[onfcsi] ~ IndTab$paint[onfcsi], rho = 0)
 survdiff(IndTab$Surv[onfctw] ~ IndTab$paint[onfctw], rho = 0)
@@ -492,5 +660,51 @@ ictest(IndTab$Surv2[oefftw] ~ IndTab$paint[oefftw], scores = "logrank1")
 
 #right censored data only?
 
-#Interval Censored
-ictest()
+###############################################################################
+#Cluster considered
+
+#Conventional Log-rank
+survdiff(IndTab$Surv[onfcon] ~ IndTab$paint[onfcon]+ cluster(IndTab$TreatQuad[onfcon]))
+survdiff(IndTab$Surv[onfcth] ~ IndTab$paint[onfcth]+ cluster(IndTab$TreatQuad[onfcth]))
+survdiff(IndTab$Surv[onfcsi] ~ IndTab$paint[onfcsi]+ cluster(IndTab$TreatQuad[onfcsi]))
+survdiff(IndTab$Surv[onfctw] ~ IndTab$paint[onfctw]+ cluster(IndTab$TreatQuad[onfctw]))
+#oncctw: One day, clorfenapyr, 24 hours exposure, 
+survdiff(IndTab$Surv[oncctw] ~ IndTab$paint[oncctw]+ cluster(IndTab$TreatQuad[oncctw]))
+
+survdiff(IndTab$Surv[nifcon] ~ IndTab$paint[nifcon]+ cluster(IndTab$TreatQuad[nifcon]))
+survdiff(IndTab$Surv[nifcth] ~ IndTab$paint[nifcth]+ cluster(IndTab$TreatQuad[nifcth]))
+survdiff(IndTab$Surv[nifcsi] ~ IndTab$paint[nifcsi]+ cluster(IndTab$TreatQuad[nifcsi]))
+survdiff(IndTab$Surv[nifctw] ~ IndTab$paint[nifctw]+ cluster(IndTab$TreatQuad[nifctw]))
+survdiff(IndTab$Surv[nicctw] ~ IndTab$paint[nicctw]+ cluster(IndTab$TreatQuad[nicctw]))
+
+survdiff(IndTab$Surv[oefcon] ~ IndTab$paint[oefcon]+ cluster(IndTab$TreatQuad[oefcon]))
+survdiff(IndTab$Surv[oefcth] ~ IndTab$paint[oefcth]+ cluster(IndTab$TreatQuad[oefcth]))
+survdiff(IndTab$Surv[oefcsi] ~ IndTab$paint[oefcsi]+ cluster(IndTab$TreatQuad[oefcsi]))
+survdiff(IndTab$Surv[oefctw] ~ IndTab$paint[oefctw]+ cluster(IndTab$TreatQuad[oefctw]))
+survdiff(IndTab$Surv[oecctw] ~ IndTab$paint[oecctw]+ cluster(IndTab$TreatQuad[oecctw]))
+
+#Now lets compare clorfenapry to 5A-IGR directly
+survdiff(IndTab$Surv[onfftw] ~ IndTab$paint[onfftw]+ cluster(IndTab$TreatQuad[onfftw]))
+survdiff(IndTab$Surv[nifftw] ~ IndTab$paint[nifftw]+ cluster(IndTab$TreatQuad[nifftw]))
+survdiff(IndTab$Surv[oefftw] ~ IndTab$paint[oefftw]+ cluster(IndTab$TreatQuad[oefftw]))
+###############################################################################
+#Let's consider that the data for every exposure is repeated 4 times 
+#on 4 different painted quadrants.
+
+IndTab$days.after.paint <- factor(IndTab$days.after.paint)
+IndTab$TreatQuad <- factor(IndTab$TreatQuad)
+
+coxm<-coxph(IndTab$Surv ~ IndTab$days.after.paint+IndTab$exp.time+ IndTab$paint +cluster(IndTab$TreatQuad))
+stargazer(coxm, type ="html", out = "cox_model.html")
+
+stm <- surv_test(IndTab$Surv[oefcon] ~ IndTab$paint[oefcon])
+
+ictest(IndTab$Surv2[onfftw] ~ IndTab$paint[onfftw] + cluster(IndTab$TreatQuad[onfftw]), scores = "logrank1")
+
+survdiff(IndTab$Surv[onfcon] ~ IndTab$paint[onfcon]+ cluster(IndTab$TreatQuad[onfcon]))
+
+
+stargazer(coxm, type ="html", out = "cox_model.html")
+
+
+
